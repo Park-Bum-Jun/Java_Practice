e577b2aae5374d7d28a725c5d790bd86c8594a14
ParkBumJun/CBNU SW <qkqjwn12@naver.com>
2017-07-10 14:09:45 +0900

    Swing Practice

---
+++ 20170710/src/Swing/JFrameTest.java
@@ -0,0 +1,19 @@
+package Swing;
+
+import javax.swing.*;
+
+public class JFrameTest extends JFrame{
+
+	public JFrameTest()
+	{
+		super("J프레임연습");
+		setSize(260,100);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrameTest f = new JFrameTest();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
